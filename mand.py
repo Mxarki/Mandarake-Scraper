d3aabb33fc990674941fa67bd96bc5d26ab79c15
Mxarki <78881074+Mxarki@users.noreply.github.com>
2023-05-15 00:17:32 -0400

    Rename file, Now reads different characters, Parse Error Handling, Etc
    Added Logger function to output both console and webhook.
    Added better HTML parsing error handling (Will return values as unknown if not found).
    Added better error handling.
    Fixed configParser not being able to read urls in japanese.
    Fixed other bugs.

--- mand.py
+++ mand.py
@@ -7,10 +7,16 @@ import os
 import json
 import bs4
 
+#Log function
+def logger(x):
+    print(x)
+    log_message = {"content": x}
+    requests.post(log_webhook_url, json=log_message)
+
 # Config parser
 config = ConfigParser()
 if os.path.isfile("config.ini"):
-    config.read("config.ini")
+    config.read("config.ini", encoding='utf-8')
     urls_str = config["Directory"]["urls"]
     urls = [url.strip().strip('"') for url in urls_str.strip('][').split(',')]
     check_interval = int(config["Function"]["check_interval"].strip('"'))
@@ -26,25 +32,23 @@ else:
 # Load past items from text file
 past_items = set()
 if os.path.isfile(past_items_file):
-    print("\n[Console] past_items.txt found!")
+    logger("\n[Console] past_items.txt found!")
     with open(past_items_file, "r") as f:
         for line in f:
             past_items.add(line.strip())
 else:
-    print("\n[Console] past_items.txt not found! Exiting...")
+    logger("\n[Console] past_items.txt not found! Exiting...")
     exit()
 
 # Loop indefinitely
-print("[Console] Starting search loop with URLs...\n")
+logger("[Console] Starting search loop with URLs...\n")
 while True:
     try:
         for url in urls:
             response = requests.get(url)
             # log to console and log webhook
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"[{timestamp}] Checking {url}")
-            log_message = {"content": f"[{timestamp}] Checking {url}"}
-            requests.post(log_webhook_url, json=log_message)
+            logger(f"[{timestamp}] Checking {url}")
 
             # check for new items
             items = re.findall(r'/order/detailPage/item\?itemCode=(\d+)', response.text)
@@ -52,29 +56,57 @@ while True:
             for item_id in items:
                 item_url = f"https://order.mandarake.co.jp/order/detailPage/item?itemCode={item_id}&lang={webhook_lang}"
                 if item_id not in past_items:
-                    # Parse the Title HTML data
+                    #Parse the Title HTML data
                     title = "Title Unknown"
                     result = requests.get(item_url)
                     soup = bs4.BeautifulSoup(result.text, "lxml")
                     title = soup.select('title')[0].getText()
 
                     # Parse the Image HTML data
-                    pinterest_link = soup.select_one('a[href^="https://www.pinterest.com/"]')
-                    if pinterest_link:
-                        pinterest_url = pinterest_link['href']
-                        media_param = pinterest_url.split('&')[1]
-                        img_url = media_param.split('=')[1]
+                    img_url = "https://i.imgur.com/p543OMk.jpg"
+                    try:
+                        pinterest_link = soup.select_one('a[href^="https://www.pinterest.com/"]')
+                        if pinterest_link:
+                            pinterest_url = pinterest_link['href']
+                            media_param = pinterest_url.split('&')[1]
+                            img_url = media_param.split('=')[1]
+                        else:
+                            logger(f"[Console] @ [{timestamp}] Imaage not found! Ignoring...")
+                    except Exception as e:
+                        logger(f"[Console] @ [{timestamp}] Error parsing Image: {e}")
 
                     # Parse the item Condition
-                    condition_div = soup.find("tr", {"class": "condition"})
-                    condition_text = condition_div.find("td").text.strip()
+                    condition_text = "Condition Unknown"
+                    try:
+                        condition_div = soup.find("tr", {"class": "condition"})
+                        if condition_div:
+                            condition_text = condition_div.find("td").getText()
+                        else:
+                            logger(f"[Console] @ [{timestamp}] Condition not found! Ignoring...")
+                    except Exception as e:
+                        logger(f"[Console] @ [{timestamp}] Error parsing condition: {e}")
 
                     # Parse the item Price
-                    price = soup.find(itemprop="price")["content"]
+                    price = "Price Uknown"
+                    try:
+                        price_parse = soup.find(itemprop="price")["content"]
+                        if price_parse:
+                            price = price_parse
+                        else:
+                            logger(f"[Console] @ [{timestamp}] Price not found! Ignoring...")
+                    except Exception as e:
+                        logger(f"[Console] @ [{timestamp}] Error parsing Price: {e}")
 
                     # Parse the items store
-                    detail_panel = soup.find("div", {"class": "detail_panel", "itemprop": "offers"})
-                    store = detail_panel.find("p", {"class": "__shop"}).text.strip()
+                    store = "Store Unkown"
+                    try:
+                        detail_panel = soup.find("div", {"class": "detail_panel", "itemprop": "offers"})
+                        if detail_panel:
+                            store = detail_panel.find("p", {"class": "__shop"}).text.strip()
+                        else:
+                            logger(f"[Console] @ [{timestamp}] Store not found! Ignoring...")
+                    except Exception as e:
+                        logger(f"[Console] @ [{timestamp}] Error parsing Store: {e}")
 
                     # send alert webhook
                     message = {
@@ -125,12 +157,10 @@ while True:
                     time.sleep(1)
 
         if check_interval != 0:
-            print(f"\n[Console] Waiting {check_interval} seconds...")
+            logger(f"\n[Console] Waiting {check_interval} seconds...")
             time.sleep(check_interval)
 
     except Exception as e:
         # log to console and log webhook if there's an error
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"[Console] @ [{timestamp}] Error: {e}")
-        log_message = {"content": f"[{timestamp}] Error: {e}"}
-        requests.post(log_webhook_url, json=log_message)
+        logger(f"[Console] @ [{timestamp}] Error: {e}")
